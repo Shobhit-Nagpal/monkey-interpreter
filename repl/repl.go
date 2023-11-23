b280302e238b9fdbe10f97008daf32668a5901e8
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-11-23 23:17:33 +0530

    updated rlpl to rppl

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
     "fmt"
     "io"
     "monkey-interpreter/lexer"
-    "monkey-interpreter/token"
+    "monkey-interpreter/parser"
 )
 
 const PROMPT = ">>"
@@ -24,9 +24,38 @@ func Start(in io.Reader, out io.Writer) {
 
         line := scanner.Text()
         l := lexer.New(line)
+        p := parser.New(l)
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Printf("%+v\n", tok)
+        program := p.ParseProgram()
+
+        if len(p.Errors()) != 0 {
+            printParseErrors(out, p.Errors())
+            continue
         }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
+
+    }
+}
+
+
+const MONKEY_TEXT = `
+ /$$      /$$ /$$   /$$ /$$   /$$ /$$   /$$
+| $$$    /$$$| $$  | $$| $$$ | $$| $$  /$$/
+| $$$$  /$$$$| $$  | $$| $$$$| $$| $$ /$$/ 
+| $$ $$/$$ $$| $$  | $$| $$ $$ $$| $$$$$/  
+| $$  $$$| $$| $$  | $$| $$  $$$$| $$  $$  
+| $$\  $ | $$| $$  | $$| $$\  $$$| $$\  $$ 
+| $$ \/  | $$|  $$$$$$/| $$ \  $$| $$ \  $$
+|__/     |__/ \______/ |__/  \__/|__/  \__/
+`
+
+func printParseErrors(out io.Writer, errors []string) {
+    io.WriteString(out, MONKEY_TEXT)
+    io.WriteString(out, "Whoopsy Daisy! Ran into an error :(\n")
+    io.WriteString(out, "Parser errors:\n")
+    for _, msg := range errors {
+        io.WriteString(out, "\t" + msg + "\n")
     }
 }
