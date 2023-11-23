969860e257fde1ecf34fb6636ba1e66e5ac95a95
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-11-23 22:44:44 +0530

    parser parses let and return statements

--- parser/parser.go
+++ parser/parser.go
@@ -120,7 +120,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
         return nil
     }    
 
-    for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+
+    stmt.Value = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
@@ -132,7 +136,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
     p.nextToken()
 
-    for !p.curTokenIs(token.SEMICOLON) {
+    stmt.ReturnValue = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
